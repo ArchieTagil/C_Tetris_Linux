722e3e07da2ecda7aef464f7bb0181de301151d4
greybaca <greybaca@student.21-school.ru>
2024-04-19 16:11:10 +0500

    start to do documentation

--- src/brick_game/tetris/tetris.h
+++ src/brick_game/tetris/tetris.h
@@ -1,31 +1,24 @@
 #ifndef DEFINES_TETRIS
 #define DEFINES_TETRIS
 
-/**
- * @brief Entry point
- *
- * Execution of the program
- * starts here.
- *
- * @param argc Number of arguments
- * @param argv List of arguments
- *
- * @return Program exit status
- */
-
 #include <ncurses.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
 
+/// @brief Tetris field Heigh. As was said in the requirement
 #define ROWS_COUNT 20
+/// @brief Tetris field Width. As was said in the requirement
 #define COLS_COUNT 10
 
+/// @brief Figure field Heigh.
 #define FIGURE_ROWS 4
+/// @brief Figure field Width.
 #define FIGURE_COLS 4
 
-typedef enum { //fsm states
+/// @brief FSM States
+typedef enum {
     BEFORE_START,
     SPAWN,
     MOVING,
@@ -34,27 +27,41 @@ typedef enum { //fsm states
     EXIT_STATE
 } tetris_state;
 
+/** @brief Avaliable user actions: \n
+ * ENTER - Start (Start the game) \n
+ * P - Pause (Pause Game) \n
+ * ESC - Terminate (exit the program) \n
+ * LEFT ROW - Left (move left) \n
+ * RIGHT ROW - Right (move right) \n
+ * UP ROW - Up (Do nothing) \n
+ * DOWN ROW - Down (speed up figure) \n
+ * SPACE - Action (Rotate Figure)
+ */
 typedef enum {
-    Start, //ENTER
-    Pause, //P
-    Terminate, //ESC
-    Left, //ROW
-    Right, //ROW
-    Up, //ROW
-    Down, //ROW
-    Action //SPACE
+    Start, /*!< ENTER - Start the game */
+    Pause, /*!< P - Pause Game */
+    Terminate, /*!< ESC - exit the program */
+    Left, /*!< LEFT ROW - move left */
+    Right, /*!< RIGHT ROW - move right */
+    Up, /*!< UP ROW - Do nothing */
+    Down, /*!< DOWN ROW speed up figure */
+    Action /*!< SPACE - Rotate Figure */
 } UserAction_t;
 
+/// @brief Main structure from the task. It shows current situation in the game
 typedef struct {
-    int **field;
-    int **next;
-    int score;
-    int high_score;
-    int level;
-    int speed;
-    int pause;
+    int **field; /*!< Pointer to matrix with field of the game with current state on it */
+    int **next; /*!< Keep pointer to the matrix with next figure */
+    int score; /*!< Current score of the game */
+    int high_score; /*!< Best score of the game */
+    int level; /*!< Current difficulty level */
+    int speed; /*!< Current figure  speed */
+    int pause; /*!< Is the game on pause or not (boolean) */
 } GameInfo_t;
 
+/** @brief Additional structure 
+ * 
+ */
 typedef struct {
     int **figure_field;
     int x, y; //верхний левый угол
@@ -68,7 +75,9 @@ typedef struct {
 void userInput(UserAction_t action, int hold);
 GameInfo_t updateCurrentState();
 
-void print_next_figure();
+///@brief Allocate memory for game_info fields and for current figure fields. Also it fills the fields with zero value.
+///@param GameInfo_t function takes one parametr - object of GameInfo_t and change it.
+///@return return nothing.
 void game_info_init(GameInfo_t *game_info);
 GameInfo_t *getInstance_GameInfo();
 void alloc_and_init_multidimensional_matrix(int ***matrix, int rows, int cols);
@@ -79,12 +88,8 @@ void figure_pointer_copy(int ***dst, int **src);
 void get_random_next_figure();
 figure *get_figure();
 void place_figure_on_the_field(GameInfo_t *game_info, figure current_figure);
-// void shift_left(figure *current_figure);
-// void shift_right(figure *current_figure);
 void shift_down(figure *current_figure);
-// void shift_up(figure *current_figure);
 int is_collision(figure current_figure, GameInfo_t game_info);
-// void print_matrix(int **matrix, int r, int c);
 void matrix_pointer_copy(int ***dst, int **src, int r, int c);
 void figures_init(figure *current_figure);
 int get_figure_bottom (figure current_figure);
